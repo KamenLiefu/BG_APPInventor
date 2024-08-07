ebb7be935a224b3221f92b5963c56b32ae105945
MMMX <112626038+KamenLiefu@users.noreply.github.com>
2024-08-07 20:15:13 +0800

    Add files via upload

---
+++ AppServer.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+'/home/kali/Desktop/appinventor-sources/appinventor/appengine-java-sdk/bin/dev_appserver.sh'  --port=8888 --address=0.0.0.0 '/home/kali/Desktop/appinventor-sources/appinventor/appengine/build/war' 
+
---
+++ StartServer.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd '/home/kali/Desktop/appinventor-sources/appinventor/buildserver' && ant RunLocalBuildServer
+
+
